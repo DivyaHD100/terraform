df030c42e2899bc37187ad82cd05b83984f7ff18
tejash raju <tejashraju@tejashs-Air.lan>
2023-04-26 18:40:38 -0400

    Autopush

--- examples/ec2.tf
+++ examples/ec2.tf
@@ -1,11 +1,11 @@
-provider "aws"{
+provider "aws" {
     region = "us-east-1"
 }
 
 # Block to create EC2
 resource "aws_instance" "web" {
   ami                       = "ami-0d627f07755afa83c"
-  instance_type             = "t3.nano"
+  instance_type             = "t3.micro"
   #vpc_security_group_ids    = [aws_security_group.allow_ssh_sg.id]
 
   tags = {
